ac385e8ecdf31fb78c474c36003fc4a3b7637b52
Andrew S Wong <andrewwong2005@gmail.com>
2026-05-24 21:17:58 -0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.go
@@ -0,0 +1,22 @@
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	lpt := 0
+	rpt := len(s) - 1
+	for {
+		if lpt + 1 > rpt {
+			break
+		}
+		l := rune(s[lpt]); r := rune(s[rpt])
+		if !unicode.IsLetter(l) && !unicode.IsDigit(l) {
+			lpt++
+		} else if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+			rpt--
+		} else if l == r {
+			lpt++
+			rpt--
+		} else {
+			return false
+		}
+	}
+	return true
+}
