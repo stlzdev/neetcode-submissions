1098b587e3611f23da8d89d11f379347d495e045
Andrew Stellis Wong <andrewwong2005@gmail.com>
2026-06-01 09:27:42 -0600

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.go
@@ -0,0 +1,24 @@
+func isValid(s string) bool {
+	stack := []rune{}
+	if len(s) == 1 {
+		return false
+	}
+	pair := map[rune]rune {
+		')' : '(',
+		'}' : '{',
+		']': '[',
+	}
+	for _, r := range s {
+		if r == '(' || r == '{' || r == '[' {
+			stack = append(stack, r)
+		} else if len(stack) > 0 && stack[len(stack) - 1] == pair[r] {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	if len(stack) == 0 {
+		return true
+	}
+	return false
+}
