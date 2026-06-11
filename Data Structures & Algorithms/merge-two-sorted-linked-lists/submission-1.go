c15d0c5b69ef25e9026ec2e7149a340f40634d17
Andrew Stellis Wong <andrewwong2005@gmail.com>
2026-06-10 20:07:34 -0600

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    curr := dummy
+    for list1 != nil && list2 != nil {
+        if list1.Val < list2.Val {
+            curr.Next = list1
+            list1 = list1.Next
+        } else {
+            curr.Next = list2
+            list2 = list2.Next
+        }
+        curr = curr.Next
+    }
+    if list1 == nil {
+        curr.Next = list2
+    } 
+    if list2 == nil {
+        curr.Next = list1
+    }
+    return dummy.Next
+}
