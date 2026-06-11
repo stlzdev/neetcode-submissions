eed912a607df376a8c5ea8dffd6ea8157a0ccddb
Andrew Stellis Wong <andrewwong2005@gmail.com>
2026-06-10 20:34:10 -0600

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func hasCycle(head *ListNode) bool {
+    if head == nil {
+        return false
+    }
+    fast := head
+    slow := head
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+        if slow == fast {
+            return true
+        }
+    }
+    return false
+}
