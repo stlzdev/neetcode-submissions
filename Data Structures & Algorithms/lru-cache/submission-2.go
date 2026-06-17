e111a65cf4f5e5e5ca9f2a0417e61d6dc644ad26
Andrew Stellis Wong <andrewwong2005@gmail.com>
2026-06-17 09:18:28 -0600

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.go
@@ -0,0 +1,63 @@
+type LRUCache struct {
+    hmap map[int]*ListNode
+	head *ListNode
+	tail *ListNode
+	capacity int
+}
+
+type ListNode struct {
+	Key int
+	Val int
+	Prev *ListNode
+	Next *ListNode
+}
+
+func Constructor(capacity int) LRUCache {
+	head := &ListNode{}
+	tail := &ListNode{}
+	head.Next = tail
+	tail.Prev = head
+	return LRUCache{
+		hmap: make(map[int]*ListNode, capacity),
+		head: head,
+		tail: tail,
+		capacity: capacity,
+	}
+}
+
+func (this *LRUCache) remove(node *ListNode) {
+	prev, nxt := node.Prev, node.Next
+	prev.Next, nxt.Prev = nxt, prev
+}
+
+func (this *LRUCache) insert(node *ListNode) {
+	prev, nxt := this.tail.Prev, this.tail
+	prev.Next, nxt.Prev = node, node
+	node.Next, node.Prev = nxt, prev
+}
+
+func (this *LRUCache) Get(key int) int {
+    if node, exists := this.hmap[key]; exists {
+		this.remove(this.hmap[key])
+		this.insert(this.hmap[key])
+		return node.Val
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+    if node, exists := this.hmap[key]; exists {
+		node.Val = value
+		this.remove(node)
+		this.insert(node)
+		return
+	}
+	node := &ListNode{Key: key, Val: value}
+	this.hmap[key] = node
+	this.insert(node)
+	if len(this.hmap) > this.capacity {
+		lru := this.head.Next
+		this.remove(lru)
+		delete(this.hmap, lru.Key)
+	}
+}
