2e4dc0330b049a5d1cd8b230c460e17e4ee4348a
Andrew Stellis Wong <andrewwong2005@gmail.com>
2026-07-01 20:42:24 -0600

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func rightSideView(root *TreeNode) []int {
+    if root == nil {
+        return []int{}
+    }
+    out := []int{}
+    queue := list.New()
+    queue.PushBack(root)
+    for queue.Len() > 0 {
+        var vis int
+        llen := queue.Len()
+        for i := 0; i < llen; i++ {
+            front := queue.Front()
+            el := front.Value.(*TreeNode)
+            queue.Remove(front)
+            vis = el.Val
+            if el.Left != nil {
+                queue.PushBack(el.Left)
+            }
+            if el.Right != nil {
+                queue.PushBack(el.Right)
+            }
+        }
+        out = append(out, vis)      
+    }
+    return out
+}
