b0fa6a352f77b987917bfa68df7a839337fc3ee8
Andrew Stellis W. <andrewwong2005@gmail.com>
2026-07-27 21:52:43 -0600

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.go
@@ -0,0 +1,30 @@
+import (
+	"slices"
+)
+func subsetsWithDup(nums []int) [][]int {
+	out := [][]int{}
+	slices.Sort(nums)
+	var add func([]int, int)
+	add = func(curr []int, idx int) {
+		if idx == len(nums) {
+			newCurr := append([]int{}, curr...)
+			out = append(out, newCurr)
+			return
+		} 
+		// include current index element
+		curr = append(curr, nums[idx])
+		add(curr, idx + 1)
+		curr = curr[:len(curr) - 1]
+		// skip current index element
+		nxt := idx + 1
+		for nxt < len(nums) {
+			if nums[nxt] != nums[idx] {
+				break
+			}
+			nxt += 1
+		}
+		add(curr, nxt)
+	}
+	add([]int{}, 0)
+	return out
+}
