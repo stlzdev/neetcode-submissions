f6aed7e1c89d5e566b97eeb1d6a8252f09738848
Andrew Stellis W. <andrewwong2005@gmail.com>
2026-08-03 23:01:32 -0600

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.go
@@ -0,0 +1,48 @@
+type WordDictionary struct {
+	children map[byte]*WordDictionary
+    isEnd bool
+}
+
+func Constructor() *WordDictionary {
+    return &WordDictionary {
+        children: make(map[byte]*WordDictionary),
+        isEnd: false,
+    }
+}
+
+func (this *WordDictionary) AddWord(word string)  {
+    for _, c := range word {
+        if _, exist := this.children[byte(c)]; !exist {
+            this.children[byte(c)] = &WordDictionary {
+                children: make(map[byte]*WordDictionary),
+                isEnd: false,
+            }
+        }
+        this = this.children[byte(c)]
+    }
+    this.isEnd = true
+}
+
+func (this *WordDictionary) Search(word string) bool {
+    return dfs(this, word)
+}
+
+func dfs(node *WordDictionary, word string) bool {
+    if word == "" {
+        return node.isEnd
+    }
+    c := word[0]
+    if c == '.' {
+        for _, child := range node.children {
+            if dfs(child, word[1:len(word)]) {
+                return true
+            }
+        }
+        return false
+    } else {
+        if _, exist := node.children[byte(c)]; !exist {
+            return false
+        }
+        return dfs(node.children[byte(c)], word[1:len(word)])
+    }
+}
