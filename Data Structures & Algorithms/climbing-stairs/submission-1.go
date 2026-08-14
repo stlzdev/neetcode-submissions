b2c31d8445e5c881ecdaaec4ac9b169f43432b14
Andrew Stellis W. <andrewwong2005@gmail.com>
2026-08-14 12:52:07 +0800

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.go
@@ -0,0 +1,14 @@
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	} else if n == 2 {
+		return 2
+	}
+	dp := make(map[int]int, n + 1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i < n + 1; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
