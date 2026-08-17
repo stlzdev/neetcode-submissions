b0a1b4a40c16772a542f60dd5fcc9eaf6301bc44
Andrew Stellis W. <andrewwong2005@gmail.com>
2026-08-17 23:04:03 +0800

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.go
@@ -0,0 +1,20 @@
+func countSubstrings(s string) int {
+    if len(s) == 1 {
+		return 1
+	}
+	count := 0
+	var expand func(int, int) int
+	expand = func(start int, end int) int {
+		for start >= 0 && end < len(s) && s[start] == s[end] {
+			count += 1
+			start -= 1
+			end += 1
+		}
+		return end - start + 1
+	}
+	for i := range len(s) {
+		expand(i, i)
+		expand(i, i+1)
+	}
+	return count
+}
