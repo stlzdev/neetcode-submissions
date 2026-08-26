7594fe6f03677dddc6080c1907dd26f72adbecb7
Andrew Stellis W. <andrewwong2005@gmail.com>
2026-08-26 09:14:15 -0400

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.go
@@ -0,0 +1,58 @@
+import "slices"
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+    graph := make([][]int, numCourses)   // adjacency list
+    state := make([]int, numCourses)     // 0=unvisited, 1=visiting, 2=visited
+
+    // Step 1: build the graph (same as BFS)
+	for _, el := range prerequisites {
+		graph[el[1]] = append(graph[el[1]], el[0])
+	}
+
+    res := []int{}
+    hasCycle := false
+
+    // Step 2: define the dfs closure
+    var dfs func(course int)
+    dfs = func(course int) {
+        // base case: visiting -> cycle detected
+		if state[course] == 1 {
+			hasCycle = true
+			return
+		}
+
+        // base case: visited -> already done, return
+		if state[course] == 2 {
+			return
+		}
+
+        // mark visiting, recurse into neighbors
+		state[course] = 1
+		for _, node := range graph[course] {
+			dfs(node)
+			if hasCycle {
+				return
+			}
+		}
+
+        // mark visited, append to order
+		state[course] = 2
+		res = append(res, course)
+    }
+
+    // Step 3: drive dfs from every unvisited course
+	for node, curr := range state {
+		if curr == 0 {
+			dfs(node)
+		}
+	}
+
+    // Step 4: if cycle, return empty
+	if hasCycle {
+		return []int{}
+	}
+
+    // Step 5: reverse order, return
+	slices.Reverse(res)
+	return res
+}
