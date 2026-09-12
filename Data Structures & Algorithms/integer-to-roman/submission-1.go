9852a8c684a2d329bd97c21ab5d70c7f1237bcfd
Andrew Stellis W. <andrewwong2005@gmail.com>
2026-09-12 15:10:51 -0400

    Add: integer-to-roman - submission-1

---
+++ Data Structures & Algorithms/integer-to-roman/submission-1.go
@@ -0,0 +1,14 @@
+func intToRoman(num int) string {
+    vals := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	syms := []string{"M", "CM", "D", "CD", "C", "XC", 
+	"L", "XL", "X", "IX", "V", "IV", "I"}
+	var b strings.Builder
+	b.Grow(15)
+	for i := 0; i < len(vals); i++ {
+		for num >= vals[i] {
+			b.WriteString(syms[i])
+			num -= vals[i]
+		}
+	}
+	return b.String()
+}
